91dfb1fcb6642636c1f7243245395324a84cee4c
Piyush L. Shrestha <piyushshrestha20@gmail.com>
2020-09-05 20:10:29 +0545

    Final Edits.

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,18 +1,40 @@
 function switch_tabs(element_reference, class_name){
     
-    // Firstly removing tab-open class from all the elements of the related group(specified by the other group class name), and adding tab-close class in them. 
-    $('.'+class_name).removeClass('tab-open');
-    $('.'+class_name).addClass('tab-close');
-    
-    // Removing tab-close class from the clicked element(TAB HEADING), and adding tab-open class in it.
-    $(element_reference).removeClass('tab-close');
-    $(element_reference).addClass('tab-open');
+    const active_element = $('.tab-open');
+    const active_element_id = getIDFromHref(active_element[0].href);
 
-    // Getting the ID of the TAB CONTENT, and removing tab-close class from the TAB CONTENT, and adding tab-open class in it.
+    element_reference.classList.remove('tab-close');
+    element_reference.classList.add('tab-open');
+
     var ID = getIDFromHref(element_reference.href);
     $('#'+ID).removeClass('tab-close');
     $('#'+ID).addClass('tab-open');
-    // document.getElementById(ID).style.transition = 'all 2s';
+
+    active_element[0].classList.remove('tab-open');
+    active_element[0].classList.add('tab-close');
+
+    // const active_element_id = getIDFromHref(active_element[0].href);
+    $('#'+active_element_id).removeClass('tab-open');
+    $('#'+active_element_id).addClass('tab-close');
+    
+
+    // alert(getIDFromHref(active_element[0].href))
+
+    // active_element.classList.remove('tab-open');
+
+    // // Firstly removing tab-open class from all the elements of the related group(specified by the other group class name), and adding tab-close class in them. 
+    // $('.'+class_name).removeClass('tab-open');
+    // $('.'+class_name).addClass('tab-close');
+    
+    // // Removing tab-close class from the clicked element(TAB HEADING), and adding tab-open class in it.
+    // $(element_reference).removeClass('tab-close');
+    // $(element_reference).addClass('tab-open');
+
+    // // Getting the ID of the TAB CONTENT, and removing tab-close class from the TAB CONTENT, and adding tab-open class in it.
+    // var ID = getIDFromHref(element_reference.href);
+    // $('#'+ID).removeClass('tab-close');
+    // $('#'+ID).addClass('tab-open');
+    // // document.getElementById(ID).style.transition = 'all 2s';
 
 }
 
@@ -78,6 +100,6 @@ $(window).on('load', function(){
     $('#screen-loader').fadeOut('slow');
     // window.setTimeout(function(){
     //     $('#screen-loader').fadeOut('slow');
-    // }, 500);
+    // }, 1500);
     document.body.style.overflowY = 'scroll';
 });
